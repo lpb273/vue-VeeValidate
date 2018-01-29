b179e41aa9ba9371b960cb3538760202ef412f08
liupengbo <273701801@qq.com>
2018-01-29 16:14:42 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# vue-veevalidate
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+## Tip:
+在新版本veevalidate中如果要使用中文的错误提示信息必须下载一个新的依赖
+```
+npm install vue-i18n --save
+```
+vee-validate官网：
+http://vee-validate.logaretm.com/
+vue-i18n官网
+http://kazupon.github.io/vue-i18n/en/
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }" name="email" type="text"
+      placeholder="Email">
+    <span v-show="errors.has('email')" class="help is-danger">
+      {{ errors.first('email') }}
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+</style>
